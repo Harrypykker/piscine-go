349011be54c700209f5a181ddb036138946b3460
Harrypykker <sanjar94@gmail.com>
2019-10-16 20:41:42 +0600

    sdfsd

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-  var i='a'
-  for i <= 'z' {
-    z01.PrintRune(i)
-    i++
-} 
-  z01.PrintRune('\n')
+	var i = 'a'
+	for i <= 'z' {
+		z01.PrintRune(i)
+		i++
+	}
+	z01.PrintRune('\n')
 }
-
