bd7614617cfb1c16ebf8880a8f566c9a1f100814
Harrypykker <sanjar94@gmail.com>
2019-10-16 22:51:14 +0600

    2-5

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for x := '0'; x <= '7'; x++ {
-		for y := x + 1; y <= '8'; y++ {
-			for z := y + 1; z <= '9'; z++ {
-				z01.PrintRune(x)
-				z01.PrintRune(y)
-				z01.PrintRune(z)
-				if x < '7' {
+	for i := '0'; i <= '9'; i++ {
+		for j := i + 1; j <= '9'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+				if i != '7' {
 					z01.PrintRune(44)
 					z01.PrintRune(32)
 				} else {
 					z01.PrintRune(10)
+				}
+			}
 		}
-	}
-}	
 	}
 }
