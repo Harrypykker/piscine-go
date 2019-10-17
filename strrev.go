5bab798655ec72779132969793a53922c61cb6a4
Harrypykker <sanjar94@gmail.com>
2019-10-17 20:54:59 +0600

    38

--- strrev.go
+++ strrev.go
@@ -2,8 +2,8 @@ package piscine
 
 func StrRev(s string) {
 	var rev string
-	for i := len(s)-1 ; i > =0; i-- {
-		rev += string(s[i])
+	for i := len(s); i >= 0; i-- {
+		rev = string(s[i])
 	}
 	return rev
 }
