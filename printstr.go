7bf77804f70f2b690ddc9cdeafac9bc3eed9fae3
Harrypykker <sanjar94@gmail.com>
2019-10-17 20:01:43 +0600

    36

--- printstr.go
+++ printstr.go
@@ -3,12 +3,6 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-
-
-	z01.PrintRune(str[0]
-	z01.PrintRune(str[1]
-	z01.PrintRune(str[2]
-	z01.PrintRune(str[3]
-	z01.PrintRune(str[4]
-	z01.PrintRune(str[5]
+	for _, r := range str {
+	z01.PrintRune(r)
 }
