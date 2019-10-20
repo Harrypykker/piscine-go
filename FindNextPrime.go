cdfba5d1fde2c4f3bf4ca0d4aec89d5b56bebf98
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-20 16:45:02 +0600

    Update FindNextPrime.go

--- FindNextPrime.go
+++ FindNextPrime.go
@@ -5,7 +5,7 @@ func FindNextPrime(nb int) int {
 		if nb%i == 0 {
 			check = check + 1
 			if check >= 2 {
-				for j := nb + 1; j < 1000000; j++ {
+				for j := nb + 1; j < nb+1000; j++ {
 					check2 := 0
 					for k := 1; k <= j; k++ {
 						if j%k == 0 {
