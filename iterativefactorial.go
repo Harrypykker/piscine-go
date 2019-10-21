1cabb7cbd31d616bb6c4292d6c5943e36a32fe02
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-21 11:40:43 +0600

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,12 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	If nb <= 0 {
-		result == 0}
-	If nb > 0 {
-	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
+	if nb > 0 {
+		result := 1
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else {
+		return 0
 	}
-	return result
 }
