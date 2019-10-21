364dc9876e83245ea5cef6eaa9e903d0a904a6e2
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-21 19:13:26 +0600

    Update isprime.go

--- isprime.go
+++ isprime.go
@@ -1,18 +1,15 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb < 0 || nb > 20 {
-		check := 0
-		for i := 1; i < nb; i++ {
-			if nb%i == 0 {
-				check = check + 1
-				if check >= 2 {
-					return false
-				}
+	check := 0
+	for i := 1; i < nb; i++ {
+		if nb%i == 0 {
+			check = check + 1
+			if check >= 2 {
+				return false
 			}
-
 		}
-		return true
+
 	}
-	return false
+	return true
 }
