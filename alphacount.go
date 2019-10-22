22482b5ff1598be348d7ea04fa580f620b3e7bf6
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-22 10:39:50 +0600

    Create alphacount.go

---
+++ alphacount.go
@@ -0,0 +1,11 @@
+package piscine
+
+import "github.com/01-edu/z01.PrintRune"
+
+func AlphaCount(str string) int {
+	for index, letter := range str {
+		z01.PrintRune("letter: %c\n")
+	}
+	z01.PrintRune(str)
+}
+
