16cf0f8e50adf24860037d33866a53ce843fb802
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-22 17:05:13 +0600

    Update printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,5 +1,33 @@
 package piscine
 
-func PrintNbrInOrder(n int) {
+import "github.com/01-edu/z01"
 
+func PrintNbrInOrder(n int) {
+	var list []int
+	length := 0
+	if n == 0 {
+		list = append(list, 0)
+		z01.PrintRune(rune(list[0]) + '0')
+	}
+	for n > 0 {
+		last := n % 10
+		n = n / 10
+		list = append(list, last)
+		length++
+	}
+	for {
+		counter := 0
+		for i := 1; i < length; i++ {
+			if list[i] < list[i-1] {
+				list[i], list[i-1] = list[i-1], list[i]
+				counter++
+			}
+		}
+		if counter == 0 {
+			break
+		}
+	}
+	for j := 0; j < length; j++ {
+		z01.PrintRune(rune(list[j]) + '0')
+	}
 }
