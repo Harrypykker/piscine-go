f2f53bfadc174eea292a31486525e434360fcd47
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-23 10:55:20 +0600

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,1 +1,13 @@
+package piscine
 
+import "os"
+import "github.com/01-edu/z01"
+
+func main() {
+	count := os.Args[0]
+	p := []rune(count)
+	for _, l := range p {
+		z01.PrintRune(l)
+	}
+	z01.PrintRune('\n')
+}
