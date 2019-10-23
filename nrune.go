f8ce72e985e321e5fc387699c5d24f18af700a4d
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-23 10:14:53 +0600

    Update nrune.go

--- nrune.go
+++ nrune.go
@@ -1,7 +1,10 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	var first rune
-	first := []rune(s)[n]
-	return 0
+	first := []rune(s)
+	if StrLen2(first) < n || n < 1 {
+		return '\x00'
+	} else {
+		return first[n-1]
+	}
 }
