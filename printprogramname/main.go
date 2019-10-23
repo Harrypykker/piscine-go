4be02d00f9657e200c8ac102d1d2e0f35effb805
Harrypykker <54544504+Harrypykker@users.noreply.github.com>
2019-10-23 19:10:29 +0600

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,13 +1,13 @@
-package piscine
+package main
 
 import "os"
 import "github.com/01-edu/z01"
 
 func main() {
-	count := os.Args[0]
-	p := []rune(count)
-	for _, l := range p {
-		z01.PrintRune(l)
+	argument := os.Args[0]
+	a := []rune(argument)
+	for _, i := range a {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
